64cd0340e168249f56c91158bd5ae669419a65e4
pg1211 <143668413+pg1211@users.noreply.github.com>
2025-01-09 16:15:18 -0500

    scrapping stuff

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/pg1211.github.io/',
-})
